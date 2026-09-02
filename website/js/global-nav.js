17cd3f573ae71b6f598efc51359fc7c530f769d5
cheenle <cheenle@users.noreply.github.com>
2026-09-02 17:58:09 +0800

    refactor(nav): FDE → Agentic 术语与 portal 总纲链接

--- website/js/global-nav.js
+++ website/js/global-nav.js
@@ -15,7 +15,7 @@
   if (!SITE) {
     var p = location.pathname.replace(/\/+$/, '');
     if (p === '' || p === '/index.html' || /(^|\/)portal\//.test(p)) SITE = 'portal';
-    else if (/\/fde\.html/.test(p)) SITE = 'fde';
+    else if (/\/agentic\.html/.test(p)) SITE = 'agentic';
     else if (/\/mrrc\//.test(p)) SITE = 'mrrc';
     else if (/\/sunmrrc\//.test(p)) SITE = 'sunmrrc';
     else if (/\/sunsdrmobile\//.test(p)) SITE = 'sunsdrmobile';
@@ -31,7 +31,7 @@
   // Absolute paths (sites live under www.vlsc.net)
   var PATHS = {
     portal: '/',
-    fde: '/fde.html',
+    agentic: '/agentic.html',
     mrrc: '/mrrc/',
     mrrc_ft710: '/mrrc_ft710/',
     sunmrrc: '/sunmrrc/',
@@ -111,7 +111,7 @@
         '<span>VLSC<span class="dot">·</span>Projects</span>' +
       '</a>' +
       '<nav class="vlsc-gn-links">' +
-        siteLink('fde', 'FDE') +
+        siteLink('agentic', 'Agentic') +
         siteLink('mrrc', 'MRRC') +
         siteLink('mrrc_ft710', 'FT-710') +
         siteLink('sunmrrc', 'SunMRRC') +
